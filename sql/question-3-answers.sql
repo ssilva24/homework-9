ad7c730a0027a5b68e742cc5717beac8c678c615
Stephanie Silva <124521846+ssilva24@users.noreply.github.com>
2024-04-07 23:56:39 -0400

    Update question-3-answers.sql

--- sql/question-3-answers.sql
+++ sql/question-3-answers.sql
@@ -1,16 +1,20 @@
 -- select by id 1:
-
+select * from post where id = 1;
 
 -- select all posts where title = includes "title 2":
-
+select * from post where title like 'title 2';
 
 -- select all posts and order by the title column alphabetically:
-
+select * from post order by name ASC;
 
 -- insert 3 new posts
-
+insert into post (name, title, content) values
+('post_1', 'Title 1', 'Post 1'),
+('post_2', 'Title 2', 'Post 2'),
+('post_3', 'Title 3', 'Post 3');
 
 -- update posts where id = 1, set the title to "new title"
-
+update post set name = 'Updated Post' where id = 1;
 
 -- delete post where id = 2
+delete from post where id = 2;
